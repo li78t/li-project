cc8c590998a4dbe35694270c862e551e60eb52e9
li78t <154497028+li78t@users.noreply.github.com>
2023-12-23 13:39:39 +0800

    Add files via upload

--- rs_interpret.m
+++ rs_interpret.m
@@ -1,4 +1,8 @@
-%得到码距最短的码字：
+<<<<<<< HEAD
+%得到码距最短的码字修改：
+=======
+%得到码距最短的码字111：
+>>>>>>> d4866c7a3d85237e172c8d51029c0fb62f9b1d8a
 function interpret_msg=rs_interpret(in_msg,all_code_msg,all_msg)
 for i=1:512
  rs_dis(i)=rscode_dis(in_msg,all_code_msg(i,:)); 
